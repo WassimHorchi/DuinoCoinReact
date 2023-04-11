80de7cf8746df8ca4e3174cd98f7c44b69b93bce
WissemChihaoui <chihahoui10@gmail.com>
2023-04-11 15:40:19 +0100

    Prepare Home page

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -4,6 +4,24 @@ function Home()
 {
     return(
         <>
+            <div class="limiter">
+                <div className='container-login100'>
+                    <div className='container'>
+                        <div className='title'>
+                            <h2>Welcome! you login as Wissem</h2>
+                        </div>
+
+                        <div className='data-container'>
+                            <div className='card-data'>
+                                <label>Wissem, you have</label>
+                                <h1>1234556</h1>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+            </div>
         </>
         
     )
